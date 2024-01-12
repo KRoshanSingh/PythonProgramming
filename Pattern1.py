6ea57d9105ba5f5e61ace665f6fabfefa51eba66
KRoshanSingh <66157127+KRoshanSingh@users.noreply.github.com>
2024-01-12 11:35:50 +0530

    Create Pattern1.py

---
+++ Pattern1.py
@@ -0,0 +1,8 @@
+row = int(input("Enter no. of rows"))
+for i in range(1,row+1):
+    for j in range(1, i+1):
+        print (j, end='')
+        
+    for k in range(i-1, 0, -1):
+        print(k, end='')
+    print()
